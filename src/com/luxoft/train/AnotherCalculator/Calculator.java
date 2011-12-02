2614a26bb81ebfefaa870e593f54deffe07afb8a
vladimir.tutov <4asovik@gmail.com>
2011-12-03 01:37:44 +0200

    place of catch errors has been changed. now we go to the main menu after error

---
+++ src/com/luxoft/train/AnotherCalculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.luxoft.train.AnotherCalculator;
+
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
